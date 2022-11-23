312ae1b470be194eaf66eca3c86c5b9a962b1fdc
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2022-11-23 11:46:47 +0100

    Creando la interfaz Clientes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Printer animalPrinter = new Printer();
+        Sociedades sociedad = new Sociedades();
+        sociedad.setId("Perro1");
+        dog.setAge(5);
+        animalPrinter.printAnimal(dog);
     }
 }
